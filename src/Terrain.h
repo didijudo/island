c11a41ca9e154eb034287e545d831f05dada40f0
Giusepe23 <giusepe@outlook.com>
2017-03-04 13:37:59 -0300

    Atualização do terreno

--- src/Terrain.h
+++ src/Terrain.h
@@ -1,6 +1,5 @@
 #include "vec3f.h"
 
-
 class Terrain {
 private:
 	int w; //Width
@@ -8,18 +7,13 @@ private:
 	float** hs; //Heights
 	Vec3f** normals;
 	bool computedNormals; //Whether normals is up-to-date
-
 public:
 	Terrain(int w2, int l2);
-
 	~Terrain();
-
 	int width();
 	int length();
-
 	void setHeight(int x, int z, float y);
 	float getHeight(int x, int z);
 	void computeNormals();
-
 	Vec3f getNormal(int x, int z);
 };
